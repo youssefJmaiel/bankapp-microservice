6e0ae55a17ed4146848ca8364e25f8e195fb19d0
youssef <youssef.jemaiel@nearteam.fr>
2026-03-13 23:53:54 +0100

    Add modification for eureka client

--- auth-service/src/main/java/com/bankapp/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/bankapp/auth/AuthServiceApplication.java
@@ -2,9 +2,11 @@ package com.bankapp.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
